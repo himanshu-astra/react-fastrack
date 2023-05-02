5bb07ee686e43a6bac9162e7e976afa15ebf0476
himanshu-astra <107385151+himanshu-astra@users.noreply.github.com>
2023-05-02 21:53:00 +0530

    day2

--- src/index.js
+++ src/index.js
@@ -3,30 +3,30 @@ import ReactDOM from "react-dom/client";
 
 const youtubeTags = ["All", "Dubbing", "Comedy", "Music", "One More"];
 
-const Tags = ({ text }) => {
+// Input of functions: props (Javascript Object)
+// Output: HTML(JSX)
+
+// Custom HTML, that you have
+// background-color => backgroundColor
+// justify-content => justifyContent
+
+// class => class
+
+// Make a Add Component, that takes in 2 arguments num1 and num2, and just shows
+// Addition is num1 + num2
+
+// <Add num1="10" num2="20"></Add>
+// Addition is 30
+
+function Greet(props) {
+  console.log(props.prop3);
+
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-      }}
-    >
-      <p style={{ color: "white" }}>{text}</p>
+    <div>
+      <p>Hello XYZ</p>
     </div>
   );
-};
+}
 
 const root = ReactDOM.createRoot(document.getElementById("abhishek"));
-root.render(
-  <div
-    style={{
-      flex: 1,
-      justifyContent: "space-between",
-      flexDirection: "row",
-      display: "flex",
-    }}
-  >
-    {youtubeTags.map((tag) => (
-      <Tags text={tag} />
-    ))}
-  </div>
-);
+root.render(<Greet prop1="Himanshu" prop2="ABC" prop3="DEF"></Greet>);
