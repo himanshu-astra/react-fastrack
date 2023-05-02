181b9892008fad945ed522efde4feadd8b71d56e
himanshu-astra <107385151+himanshu-astra@users.noreply.github.com>
2023-05-02 21:06:40 +0530

    basrebones-react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const youtubeTags = ["All", "Dubbing", "Comedy", "Music", "One More"];
+
+const Tags = ({ text }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+      }}
+    >
+      <p style={{ color: "white" }}>{text}</p>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("abhishek"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div
+    style={{
+      flex: 1,
+      justifyContent: "space-between",
+      flexDirection: "row",
+      display: "flex",
+    }}
+  >
+    {youtubeTags.map((tag) => (
+      <Tags text={tag} />
+    ))}
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
