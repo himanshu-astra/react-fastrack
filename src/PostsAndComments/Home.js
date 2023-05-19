4b70046fab7aba08fc805f0f3352622b24bd62c7
himanshu-astra <107385151+himanshu-astra@users.noreply.github.com>
2023-05-19 20:43:25 +0530

    feat: Added multiple posts

--- src/PostsAndComments/Home.js
+++ src/PostsAndComments/Home.js
@@ -1,35 +1,47 @@
 import "./Home.css";
 
-const staticData = {
-  userId: 1,
-  id: 1,
-  title:
-    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-};
+const staticData = [
+  {
+    userId: 1,
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  },
+  {
+    userId: 1,
+    id: 1,
+    title: "Title 2",
+    body: "Body 2",
+  },
+];
 
-// Step 1 of making UI
-// Make the UI as per design
-const Post = () => {
+// Step 2 of making UI
+// Make the components extensible
+const Post = (props) => {
   return (
     <div className="post-container">
-      <p className="post-title">
-        sunt aut facere repellat provident occaecati excepturi optio
-        reprehenderit
-      </p>
-      <div className="border" />
-      <pre className="post-body">
-        quia et suscipit\nsuscipit recusandae consequuntur expedita et
-        cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem
-        sunt rem eveniet architecto
-      </pre>
+      <div>
+        <p className="post-title">{props.title}</p>
+        <div className="border" />
+      </div>
+      <pre className="post-body">{props.body}</pre>
       <a href="/post">Read More...</a>
     </div>
   );
 };
 
 const Home = () => {
-  return <Post />;
+  let posts = [];
+
+  for (let i = 0; i < staticData.length; i++) {
+    const postUI = (
+      <Post title={staticData[i].title} body={staticData[i].body} />
+    );
+    posts.push(postUI);
+  }
+
+  return <div className="posts">{posts}</div>;
 };
 
 export default Home;
